4e34a3bcaa7bdc57695930f6437372bd22be2e6e
Anmol Arora <anmolarora1711@gmail.com>
2020-04-26 18:45:28 +0530

    Code

---
+++ FirstProject/src/oops/ExceptionHandling4.java
@@ -0,0 +1,67 @@
+package oops;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling4 {
+	
+	public static int correctPin = 1234;
+	
+	public static int accountBalance = 10000;
+	
+	public static Scanner scanner = new Scanner(System.in);
+	
+	public static void verifyPin(int pin) throws InputMismatchException {
+		
+		if(correctPin != pin) throw new InputMismatchException("Incorrect PIN");
+		
+	}
+	
+	public static void withdraw() throws InputMismatchException, ArithmeticException{
+		
+			System.out.println("Please enter your PIN");
+			
+			int pin = scanner.nextInt();
+			
+			verifyPin(pin);
+			
+			System.out.println("Enter amount to withdraw");
+			
+			int amount = scanner.nextInt();
+			
+			if(amount > accountBalance) throw new ArithmeticException("Amount can't be withdrawn");
+			
+			accountBalance -= amount;
+			
+			System.out.println("Remaining balance - " + accountBalance);
+
+	}
+
+	public static void main(String[] args) {
+		
+//		ExceptionHandling4 obj = new ExceptionHandling4();
+//		
+//		throw new Exception();
+		
+		while(true) {
+			System.out.println("Welcome to ABC Bank...");
+			
+			try {
+				withdraw();
+			}
+			catch(InputMismatchException e) {
+				System.out.println("InputMismatch - " + e);
+			}
+			catch(ArithmeticException e) {
+				System.out.println("Arithmetic - " + e);
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+			
+			System.out.println("Thanku for visiting us..\n");
+		}
+
+	}
+
+}
